130efea25552629c1fe6c823c6bacb0c1f5396cf
haokai li <lihaokai@amazon.com>
2017-10-30 19:24:08 +0800

    part of chapter one homework

---
+++ src/chapter1: Everything is an object/Ex1.java
@@ -0,0 +1,9 @@
+public class Ex1 {
+    public static int i;
+    public static char c;
+
+    public static void main(String[] args) {
+        System.out.println(i);
+        System.out.println(c);
+    }
+}
---
+++ src/chapter1: Everything is an object/Ex3.java
@@ -0,0 +1,10 @@
+public class Ex3 {
+    public static void main(String[] args) {
+        ATypeName a = new ATypeName();
+        System.out.println(a);
+    }
+}
+
+class ATypeName {
+    /* Class body goes here */
+}
